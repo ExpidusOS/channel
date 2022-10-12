cf4889794a3906fa7b00daecbfd05e9065266478
Tristan Ross <tross@ezoic.com>
2022-10-12 09:26:42 -0700

    chore(deploy/hydra): add the hydra config

--- deploy/hydra/configuration.nix
+++ deploy/hydra/configuration.nix
@@ -5,6 +5,14 @@
     (nixpkgs + "/nixos/modules/installer/cd-dvd/channel.nix")
   ];
 
+  services.hydra = {
+    enabled = true;
+    hydraURL = "http://hyra.expidusos.com";
+    notificationSender = "hydra@localhost";
+    buildMachinesFiles = [];
+    useSubstitutes = true;
+  };
+
   documentation.doc.enabled = false;
   system.stateVersion = "22.11";
 }
